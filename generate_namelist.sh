fca9bd0d8c8027739111d3f641eb5c50ef93fd55
quishqa <mgavidiac@gmail.com>
2019-06-26 18:31:20 -0300

    Create generate_namelist.sh
    This script creates namelist.input with different dates in order to run a long period reinitialising Meteorology with Previous Chemistry

---
+++ generate_namelist.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while read line
+do
+yi=`echo $line | cut -d ',' -f 3`
+mi=`echo $line | cut -d ',' -f 4`
+di=`echo $line | cut -d ',' -f 5`
+
+yf=`echo $line | cut -d ',' -f 6`
+mf=`echo $line | cut -d ',' -f 7`
+df=`echo $line | cut -d ',' -f 8`
+
+st=`echo $line | cut -d ',' -f 9`
+ll=`echo $line | cut -d ',' -f 9`
+file="nl${yi}${mi}${di}"
+
+sed "s/YEARI/$yi/g" template.input > $file
+sed -i "s/MONTHI/$mi/g" $file
+sed -i "s/DAYI/$di/g" $file
+sed -i "s/YEARF/$yf/g" $file
+sed -i "s/MONTHF/$mf/g" $file
+sed -i "s/DAYF/$df/g" $file
+#sed -i "s/STATUS/$st/g" $file
+
+done < dates.csv
+
+exit 0
